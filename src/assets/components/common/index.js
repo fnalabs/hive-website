09ccb1362d67c1d1afa8f34980e364154b12aeac
Adam Eilers <adam.eilers@gmail.com>
2018-09-11 19:27:21 -0700

    adding CookieConsent and GA infrastructure

--- src/assets/components/common/index.js
+++ src/assets/components/common/index.js
@@ -1,2 +1,3 @@
+export { default as AnalyticsLink } from './AnalyticsLink.jsx'
 export { default as Container } from './Container.jsx'
 export { default as Tile } from './Tile.jsx'
--- src/client/Transition.jsx
+++ src/client/Transition.jsx
@@ -1,7 +1,39 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import { withRouter } from 'react-router'
+import { Link } from 'react-router-dom'
+import CookieConsent, { Cookies } from 'react-cookie-consent'
+import ReactGA from 'react-ga'
+
+import { Provider } from '../assets/components/contexts/Consent.jsx'
+
+import meta from '../assets/metadata.json'
 
 class Transition extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isConsent: Cookies.get('CookieConsent') || false
+    }
+
+    ReactGA.initialize(process.env.GA_ID)
+  }
+
+  onAccept = () => {
+    const pageMeta = meta[this.props.location.pathname]
+    const title = pageMeta.title
+      ? `${pageMeta.title} | ${meta.common.siteName}`
+      : meta.common.siteName
+
+    ReactGA.pageview(this.props.location.pathname, undefined, title)
+    this.setState({ isConsent: true })
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    if (Cookies.get('CookieConsent')) return { isConsent: true }
+    else return null
+  }
+
   componentDidUpdate (prevProps) {
     if (this.props.location.pathname !== prevProps.location.pathname) {
       window.scrollTo(0, 0)
@@ -9,7 +41,22 @@ class Transition extends Component {
   }
 
   render () {
-    return this.props.children
+    return (
+      <Provider value={this.state}>
+        {this.props.children}
+
+        {!this.state.isConsent &&
+          <CookieConsent disableStyles
+            buttonClasses='button is-pulled-right'
+            containerClasses='notification is-hive'
+            contentClasses='is-inline-block'
+            onAccept={this.onAccept}
+          >
+            Hive<sup>io</sup> uses cookies to provide you the best experience. By clicking <strong>I Understand</strong> you are agreeing to our <Link to='/cookie'>Cookie</Link> and <Link to='/privacy'>Privacy</Link> Policies.
+          </CookieConsent>
+        }
+      </Provider>
+    )
   }
 }
 
