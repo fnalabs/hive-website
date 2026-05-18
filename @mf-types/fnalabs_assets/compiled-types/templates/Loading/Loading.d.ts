6eae54ea6a417d77b26c98816c4d10165a67a55c
Adam Eilers <adam.eilers@gmail.com>
2026-05-18 08:56:02 -0500

    chore: updating federated types

--- @mf-types/fnalabs_assets/compiled-types/templates/Loading/Loading.d.ts
+++ @mf-types/fnalabs_assets/compiled-types/templates/Loading/Loading.d.ts
@@ -1,7 +1,9 @@
 import { type FC } from 'react';
+import { type IHero } from '../../components/Hero/Hero';
 import { type IProgressBar } from '../../components/ProgressBar/ProgressBar';
 export interface ILoading {
     color?: IProgressBar['color'];
+    size?: IHero['size'];
 }
 declare const Loading: FC<ILoading>;
 export default Loading;
