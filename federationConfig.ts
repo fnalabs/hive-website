e3698cc043cfd2478be3f81a4b275f66e6e23c2e
Adam Eilers <adam.eilers@gmail.com>
2026-06-26 12:28:22 -0500

    refactor: adding Module Federation as singleton

--- federationConfig.ts
+++ federationConfig.ts
@@ -8,6 +8,7 @@ export default {
   },
   shared: {
     ...pkg.dependencies,
+    '@module-federation/enhanced': { singleton: true, requiredVersion: pkg.dependencies['@module-federation/enhanced'] },
     react: { singleton: true, requiredVersion: pkg.dependencies['react'] },
     'react-dom': { singleton: true, requiredVersion: pkg.dependencies['react-dom'] },
     'react-router': { singleton: true, requiredVersion: pkg.dependencies['react-router'] },
